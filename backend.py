cdf90f8190d9c8b76a3d16fefbc15d1b9f1da5e7
David Fries <dfries4@gmail.com>
2019-01-17 21:15:52 -0500

    added basic testing db functionality

--- backend.py
+++ backend.py
@@ -1,19 +1,42 @@
 import psycopg2
+import random
+import string
 import records
+class COPG():
+    def conn(self):
+        conn=psycopg2.connect("host=192.168.5.172 dbname=billtrak user=dj password=Skyliner34")
+        return conn
 class BTBackend():
     try:
         db=records.Database("postgresql://192.168.5.172/billtrak?user=dj&password=Skyliner34")
     except:
         print("error in db connection")
-    def getdata(self):
+
+    def genintid(self):
+        pass
+    def gencharid(self):
+        userid = ''.join(random.choices(
+            string.ascii_letters + string.digits, k=8))
+        return userid
         
-        rows=self.db.query("select * from minthill where timestamp::timestamp::date >=now()-'36 hours'::interval")
-        return rows.export('df')
+    def debugprint(self):
+        rows=self.db.query("select * from bills")
+        print(rows.first().paymenturl)
+        pass
+    
+    
     
     def getbillsbycompany(self,companyid):
-        pass
+        query="select * from bills where companyid=:companyid"
+        
+        rows=self.db.query(query,companyid=companyid)
+        return rows.all()
+        
     def getamtbybillid(self,billid):
-        pass
+        query="select * from bills where billid=:billid"
+        
+        rows=self.db.query(query,billid=billid).first()
+        return rows.amt
     def getbillinfo(self):
         pass
     def validateuser(self,userid):
@@ -26,6 +49,11 @@ class BTBackend():
         pass
     def createbill(self,companyid,amt,datepaid,dateinvoiced,paymenturl=None,phonenum=None):
         pass
-    def createnotification(self,notificationid,)
-
+    def createnotification(self,notificationid,userid,useremail,billid,amt,phonenum,paymenturl):
+        pass
+    def deletenotification(self,notificationid):
+        pass
     
+BTBackend().debugprint()
+print(BTBackend().getbillsbycompany('1') )   
+print(BTBackend().getamtbybillid('test0001'))
